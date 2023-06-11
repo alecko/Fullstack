627ec91d0978c5cebc102f24e033d25bdc7c8de1
Alejandro Maldonado <alemaldo@gmail.com>
2023-06-10 23:17:05 -0300

    Part 02 - Phonebook 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PhoneBookEntries from './components/PhoneBookEntries'
 import Filter from './components/Filter'
 import EntryForm from './components/EntryForm'
+import axios from 'axios'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    
   ])
+
+   useEffect(()=>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+   }, [])
+
+  
+  
+    
 
   const [search, setSearch] = useState('')
   const [newName, setNewName] = useState('')
